4663e3265bc051305ea851a0e56fb9b8ae837785
ongtiang <109684557+ongtiang@users.noreply.github.com>
2022-07-22 00:50:35 +0800

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer" style="background-color: rgba(255, 255, 255, 0.9);">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="">&#10146;</i> home</a>
+         <a href="about.php"> <i class="">&#10146;</i> about</a>
+         <a href="shop.php"> <i class="">&#10146;</i> shop</a>
+         <a href="contact.php"> <i class="">&#10146;</i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="">&#10146;</i> login</a>
+         <a href="user_register.php"> <i class="">&#10146;</i> register</a>
+         <a href="cart.php"> <i class="">&#10146;</i> cart</a>
+         <a href="orders.php"> <i class="">&#10146;</i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="#"><i class="">&#128222;</i> 010-7775031 </a>
+         <a href="#"><i class="">&#128222;</i> 016-4421156</a>
+         <a href="mailto:ongitang1999@gmail.com"><i class="">&#128231;</i> ongitang1999@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> 5302 Jalan Raja Uda </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/i/flow/login"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>TS Laptop</span> | all rights reserved!</div>
+
+</footer>
